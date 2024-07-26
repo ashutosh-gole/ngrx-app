c565897b484c307a413a747d65333cf559d50f2b
ashutosh-gole <ashutoshsgole@gmail.com>
2024-07-26 20:36:56 +0530

    ngrx files created

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { Product } from '../models/product';
+import { Product } from '../models/product.model';
 
 @Injectable({
   providedIn: 'root'
